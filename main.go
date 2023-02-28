d672349b18329c3b77009f4eb7eba1523bfe8596
Vincent LI <li.jiajinvincent1997@gmail.com>
2023-02-28 12:58:07 +0100

    Update main.go

--- main.go
+++ main.go
@@ -136,5 +136,6 @@ func availabilityHandler(db *sql.DB) http.HandlerFunc {
 func enableCors(w *http.ResponseWriter) {
 	(*w).Header().Set("Access-Control-Allow-Origin", "*")
 	(*w).Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE")
+	(*w).Header().Set("Access-Control-Allow-Headers", "Content-Type")
 
 }
